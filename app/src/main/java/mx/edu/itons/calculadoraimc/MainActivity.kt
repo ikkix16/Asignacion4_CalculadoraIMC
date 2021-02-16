b2a9b8caf56f26336ed4e2f9dabf2948ee873329
ikkix16 <ikkix16@gmail.com>
2021-02-15 17:41:56 -0700

    Mas perron

--- app/src/main/java/mx/edu/itons/calculadoraimc/MainActivity.kt
+++ app/src/main/java/mx/edu/itons/calculadoraimc/MainActivity.kt
@@ -2,23 +2,30 @@ package mx.edu.itons.calculadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
+    var peso: Double=0.00;
+    var estatura: Double=0.00;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /**
+         *
+
         val estatura : EditText = findViewById(R.id.idEestatura) as EditText
         val peso : EditText = findViewById(R.id.idAltura) as EditText
 
         val btnOp: Button = findViewById(R.id.btnOperacion) as Button
-
+        */
         //btnOp.setOnClickListener(View.OnClickListener { calcularIMC(estatura,peso) })
+
+
+        this.btnOperacion.setOnClickListener(){
+            idResultado.setText((calcularIMC().toString()))
+        }
 
 
 
@@ -27,12 +34,14 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun calcularIMC(estatura: Int, peso: Int): Int {
+    fun calcularIMC(): Double {
+        estatura=(this.idEestatura.text.toString().toDouble())
+        peso=(this.idPeso.text.toString().toDouble())
 
-        var imc = estatura / (peso * peso);
+        var imc = peso / (estatura * estatura)  ;
 
-
-        if (imc < 18.5) {
+        /**
+         * if (imc < 18.5) {
 
         } else if (imc > 18.6 || imc < 24.9) {
 
@@ -45,6 +54,9 @@ class MainActivity : AppCompatActivity() {
 
 
         }
+         *
+         */
+
 
         return imc
     }
